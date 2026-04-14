a85bf23140c002a608160531d6c9abe974b2013d
noahh2468 <noahhackerott@gmail.com>
2026-04-14 17:45:52 -0600

    Updates to assumptions, introduced state vector that will be used to define position and velocity of satellite in the x-y plane

--- 2bodyproblem_2d.py
+++ 2bodyproblem_2d.py
@@ -1,11 +1,19 @@
+# 2D, 2 Body Problem Orbit Simulation
+# Assumptions:
+# Earth has constant radius and is homogeneous
+# Treat Earth as a point mass with center of mass at its center (0,0)
+# x-y coordinate plane is centered at Earth's center
+
+
+
 # Parameters (all in SI Units)
-# Assuming: Earth has constant radius and homogeneous
 rad_Earth = 6378137 # meters
 mass_Earth = 5.972e24 # kg
 g_sea_level = 9.81 # m/s^2
 grav_const = 6.6743e-11 # (m^3)(kg^-1)(s^-2)
 
-
+# Define state vector
+state=[0,0,0,0] # r_x (m), r_y (m), v_x (m/s), v_y (m/s)
 
 
 # Calculation of gravitational acceleration in terms of altitude
