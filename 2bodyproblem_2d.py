c2943c2c900c6925a01a24814baf1107acade626
noahh2468 <noahhackerott@gmail.com>
2026-04-14 17:04:37 -0600

    Updates to parameters, added gravitational acceleration function (in terms of altitude)

--- 2bodyproblem_2d.py
+++ 2bodyproblem_2d.py
@@ -1,4 +1,14 @@
 # Parameters (all in SI Units)
 # Assuming: Earth has constant radius and homogeneous
-Rad_Earth = 6378137 # meters
+rad_Earth = 6378137 # meters
+mass_Earth = 5.972e24 # kg
 g_sea_level = 9.81 # m/s^2
+grav_const = 6.6743e-11 # (m^3)(kg^-1)(s^-2)
+
+
+
+
+# Calculation of gravitational acceleration in terms of altitude
+def gcalculator(altitude):
+    g = (grav_const * mass_Earth)/(rad_Earth + altitude)^2
+    return g
